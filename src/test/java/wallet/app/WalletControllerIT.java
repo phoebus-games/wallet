dbb382d58079cbe81179415d6bf1df501cae3344
Alex Collins <alex.e.c@gmail.com>
2017-02-20 14:43:35 +0000

    ignore

--- src/test/java/wallet/app/WalletControllerIT.java
+++ src/test/java/wallet/app/WalletControllerIT.java
@@ -1,10 +1,12 @@
 package wallet.app;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 
+@Ignore
 public class WalletControllerIT extends IntegrationTest {
 
     @Test
