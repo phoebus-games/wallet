d728fd14e8853574f3d47e89282352849e228fed
Alex Collins <alex.e.c@gmail.com>
2017-02-20 14:34:08 +0000

    9090

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ ssh $H "adduser --system $U || true"
 
 rsync -avz --progress target/$U-1.0.0-SNAPSHOT.jar $H:/var/helios-games/$U.jar
 
-ssh $H "chmod +x $D/$U.jar && ln -sf $D/$U.jar /etc/init.d/$U && chown $U /etc/init.d/$U && systemctl daemon-reload && systemctl start $U && systemctl restart $U"
+ssh $H "echo JAVA_OPTS=-Dserver.port=9090 > $D/$U.conf && chmod +x $D/$U.jar && ln -sf $D/$U.jar /etc/init.d/$U && chown $U /etc/init.d/$U && /etc/init.d/$U restart"
