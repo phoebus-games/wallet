a9b00b901b900c27390e5cfc4f863a54bc78da53
Alex Collins <alex.e.c@gmail.com>
2017-02-22 17:25:26 +0000

    updated config

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ ssh $H "adduser --system $U || true"
 
 rsync -avz --progress target/$U-1.0.0-SNAPSHOT.jar $H:/var/helios-games/$U.jar
 
-ssh $H "echo JAVA_OPTS=-Dserver.port=9090 > $D/$U.conf && chmod +x $D/$U.jar && ln -sf $D/$U.jar /etc/init.d/$U && chown $U /etc/init.d/$U && /etc/init.d/$U restart"
+ssh $H "echo JAVA_OPTS=\"-Dserver.port=9090 -Dspring.datasource.username=wallet -Dspring.datasource.password=wallet\" > $D/$U.conf && chmod +x $D/$U.jar && ln -sf $D/$U.jar /etc/init.d/$U && chown $U /etc/init.d/$U && /etc/init.d/$U restart"
