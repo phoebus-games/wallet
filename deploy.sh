dac0743c1793cfa7e982933d9a01a3f844277a28
Alex Collins <alex.e.c@gmail.com>
2017-02-26 19:40:36 +0000

    phoebus

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 set -eux
 
 H=root@games.alexecollins.com
-D=/var/helios-games
+D=/var/phoebus-games
 U=wallet
 
 ssh $H "[ -e $D ] || mkdir $D"
 ssh $H "adduser --system $U || true"
 
-rsync -avz --progress target/$U.jar $H:/var/helios-games/$U.jar
+rsync -avz --progress target/$U.jar $H:/var/phoebus-games/$U.jar
 
 ssh $H "echo \"JAVA_OPTS='-Dserver.port=9090 -Dspring.datasource.username=wallet -Dspring.datasource.password=wallet'\" > $D/$U.conf && chmod +x $D/$U.jar && ln -sf $D/$U.jar /etc/init.d/$U && chown $U /etc/init.d/$U && /etc/init.d/$U restart"
