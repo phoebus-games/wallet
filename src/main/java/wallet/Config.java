89cfa5fa3f43ecb1a4051f538a6b64ea1adfe6fb
Alex Collins <alex.e.c@gmail.com>
2017-02-28 15:11:12 +0000

    env config

--- src/main/java/wallet/Config.java
+++ src/main/java/wallet/Config.java
@@ -16,12 +16,24 @@ public class Config {
     @Provides
     public DataSource dataSource() {
         PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setDatabaseName("wallet");
+        ds.setUrl(System.getenv().getOrDefault("DATASOURCE_URL", "jdbc:postgresql://localhost:5432/wallet"));
+        System.getenv().forEach(
+                (key, value) -> {
+                    switch (key) {
+                        case "DATASOURCE_USERNAME":
+                            ds.setUser(value);
+                            break;
+                        case "DATASOURCE_PASSWORD":
+                            ds.setPassword(value);
+                            break;
+                    }
+                }
+        );
         return ds;
     }
 
     @Provides
-    public LiquibaseLoader liquiStrap(DataSource dataSource) {
+    public LiquibaseLoader liquibaseLoader(DataSource dataSource) {
         return new LiquibaseLoader(dataSource);
     }
 
