d4ca907b32fff89a62242d460e47a5e5ce2d4e02
Alex Collins <alex.e.c@gmail.com>
2017-02-28 14:35:51 +0000

    shade

--- src/main/java/wallet/App.java
+++ src/main/java/wallet/App.java
@@ -2,14 +2,19 @@ package wallet;
 
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
+import org.glassfish.grizzly.http.server.HttpServer;
 
 import java.io.IOException;
 import java.util.Map;
 
 public class App extends PackagesResourceConfig {
 
-    public static void main(String[] args) throws IOException {
-        GrizzlyServerFactory.createHttpServer("http://localhost:8080", new App()).start();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpServer server = GrizzlyServerFactory.createHttpServer("http://localhost:8080", new App());
+        server.start();
+        Thread.currentThread().join();
+        server.stop();
+
     }
 
     public App() {
