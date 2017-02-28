6d61a17979b089937a8760e5f3b180e0dc753229
Alex Collins <alex.e.c@gmail.com>
2017-02-28 16:25:36 +0000

    config + logging

--- src/main/java/wallet/App.java
+++ src/main/java/wallet/App.java
@@ -3,11 +3,21 @@ package wallet;
 import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import org.glassfish.grizzly.http.server.HttpServer;
+import wallet.infra.AuthFilter;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class App extends PackagesResourceConfig {
+
+    public App() {
+        super("wallet");
+        getFeatures().put("com.sun.jersey.api.json.POJOMappingFeature", true);
+        getProperties().put("com.sun.jersey.spi.container.ContainerRequestFilters",
+                //LoggingFilter.class.getName() + ";" +
+                AuthFilter.class.getName());
+        //getProperties().put("com.sun.jersey.spi.container.ContainerResponseFilters",
+        //        LoggingFilter.class.getName());
+    }
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpServer server = GrizzlyServerFactory.createHttpServer("http://localhost:8080", new App());
@@ -15,13 +25,5 @@ public class App extends PackagesResourceConfig {
         Thread.currentThread().join();
         server.stop();
 
-    }
-
-    public App() {
-        super("wallet");
-        Map<String, Boolean> features = getFeatures();
-        features.put("com.sun.jersey.api.json.POJOMappingFeature", true);
-        Map<String, Object> properties = getProperties();
-        properties.put("com.sun.jersey.spi.container.ContainerRequestFilters", "wallet.infra.AuthFilter");
     }
 }
--- src/main/java/wallet/Config.java
+++ src/main/java/wallet/Config.java
@@ -7,6 +7,9 @@ import wallet.infra.LiquibaseLoader;
 import wallet.infra.WalletRepo;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @Module(
         library = true, injects = WalletRepo.class
@@ -14,21 +17,29 @@ import javax.sql.DataSource;
 public class Config {
 
     @Provides
-    public DataSource dataSource() {
+    public Properties properties() {
+        Properties properties = new Properties();
+        try (InputStream in = Config.class.getResourceAsStream("/application.properties")) {
+            properties.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.getenv().forEach((k, v) -> {
+            properties.put(k.replaceAll("_", ".").toLowerCase(), v);
+        });
+        return properties;
+    }
+
+    @Provides
+    public DataSource dataSource(Properties properties) {
         PGSimpleDataSource ds = new PGSimpleDataSource();
-        ds.setUrl(System.getenv().getOrDefault("DATASOURCE_URL", "jdbc:postgresql://localhost:5432/wallet"));
-        System.getenv().forEach(
-                (key, value) -> {
-                    switch (key) {
-                        case "DATASOURCE_USERNAME":
-                            ds.setUser(value);
-                            break;
-                        case "DATASOURCE_PASSWORD":
-                            ds.setPassword(value);
-                            break;
-                    }
-                }
-        );
+        ds.setUrl(properties.getProperty("datasource.url"));
+        if (properties.contains("datasource.username")) {
+            ds.setUser("datasource.username");
+        }
+        if (properties.contains("datasource.password")) {
+            ds.setUser("datasource.password§");
+        }
         return ds;
     }
 
